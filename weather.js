fc3e60a555597357385e22b79cb8e704336507f7
Steve Gardner <gardnersj@gmail.com>
2015-05-21 22:19:09 -0700

    Deleted unused files, connected to Forecast.io, console.log weather

---
+++ weather.js
@@ -0,0 +1,62 @@
+//Connect to Google Geolocater API and get lat and lon with zip code
+var https = require('https');
+var address = 90405;
+
+var forecastCallback = function(lat, lng) {
+  var url = 'https://api.forecast.io/forecast/9df7ff36d0c74a57d55d88cdcff00bc9/' + lat + ',' + lng;
+  var request = https.get(url, function(response) {
+    var body = "";
+    
+    response.on('data', function(chunk) {
+      body += chunk;
+    });
+    
+    response.on('end', function() {
+      var forecast = JSON.parse(body);
+      console.log("Forecast timezone: " + forecast.timezone);
+      console.log("Forecast weather: " + forecast.currently.summary);
+    });
+  });
+}
+
+function getLoc(address) {
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyDhAB6yN4yqFEHb7Vmo30blQP6i5PemN2I";
+  var request = https.get(url, function(response) {
+      var body = "";
+
+      response.on('data', function(chunk) {
+        body += chunk;
+      });
+
+      response.on('end', function() {
+        var result = JSON.parse(body),
+            geo = result.results[0],
+            location = geo.formatted_address,
+            coord = geo.geometry.location,
+            latitude = coord.lat,
+            longitude = coord.lng;
+        
+        if (location) {
+          forecastCallback(latitude, longitude);
+        }
+        console.log("Weather for " + location);
+        console.log("Latitude: " + latitude + ". and Longitude: " + longitude);
+      });
+
+      response.on('error', function(e) {
+        console.error("Whoops! Looks like you " + e.message);
+      });
+
+  });
+}
+
+getLoc(address);
+//forecastCallback(34, -9);
+
+
+//module.exports.getLoc = getLoc;
+
+
+
+
+
